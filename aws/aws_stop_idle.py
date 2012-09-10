0592b0e1968df8c329aee7bd1705166771be780a
Chris AtLee <catlee@mozilla.com>
2012-09-10 11:57:17 -0400

    Bug 784694: import AWS load monitoring scripts

---
+++ aws/aws_stop_idle.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+"""
+Watches running EC2 instances and shuts them down when idle
+"""
+import re
+import time
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+import boto.ec2
+from paramiko import SSHClient
+import requests
+
+import logging
+log = logging.getLogger()
+
+
+def get_buildbot_instances(conn):
+    # Look for instances with moz-state=ready and hostname *-ec2-000
+    reservations = conn.get_all_instances(filters={
+        'tag:moz-state': 'ready',
+        'instance-state-name': 'running',
+    })
+
+    retval = []
+    for r in reservations:
+        for i in r.instances:
+            name = i.tags['Name']
+            if not re.match(".*-ec2-\d+", name):
+                continue
+            retval.append(i)
+
+    return retval
+
+
+class IgnorePolicy:
+    def missing_host_key(self, client, hostname, key):
+        pass
+
+
+def get_ssh_client(name, ip, passwords):
+    client = SSHClient()
+    client.set_missing_host_key_policy(IgnorePolicy())
+    for p in passwords:
+        try:
+            client.connect(hostname=ip, username='cltbld', password=p)
+            return client
+        except:
+            pass
+
+    log.warn("Couldn't log into {name} at {ip} with any known passwords".format(name=name, ip=ip))
+    return None
+
+
+def get_last_activity(name, client):
+    stdin, stdout, stderr = client.exec_command("date +%Y%m%d%H%M%S")
+    slave_time = stdout.read().strip()
+    slave_time = time.mktime(time.strptime(slave_time, "%Y%m%d%H%M%S"))
+
+    stdin, stdout, stderr = client.exec_command("cat /proc/uptime")
+    uptime = float(stdout.read().split()[0])
+
+    if uptime < 3*60:
+        # Assume we're still booting
+        log.debug("%s - uptime is %.2f; assuming we're still booting up", name, uptime)
+        return "booting"
+
+    stdin, stdout, stderr = client.exec_command("tail -n 100 /builds/slave/twistd.log.1 /builds/slave/twistd.log")
+    stdin.close()
+
+    last_activity = 0
+    running_command = False
+    t = time.time()
+    for line in stdout:
+        t = re.search("^(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})", line)
+        if t:
+            t = time.strptime(t.group(1), "%Y-%m-%d %H:%M:%S")
+            t = time.mktime(t)
+        else:
+            # Not sure what to do with this line...
+            continue
+
+        # uncomment to dump out ALL the lines
+        #log.debug("%s - %s", name, line.strip())
+
+        if "RunProcess._startCommand" in line or "using PTY: " in line:
+            log.debug("%s - started command - %s", name, line.strip())
+            running_command = True
+        elif "commandComplete" in line or "stopCommand" in line:
+            log.debug("%s - done command - %s", name, line.strip())
+            running_command = False
+
+        if "Shut Down" in line:
+            # Check if this happened before we booted, i.e. we're still booting up
+            if (slave_time - t) > uptime:
+                log.debug("%s - shutdown line is older than uptime; assuming we're still booting %s", name, line.strip())
+                last_activity = "booting"
+            else:
+                last_activity = "stopped"
+        elif running_command:
+            # We're in the middle of running something, so say that our last
+            # activity is now (0 seconds ago)
+            last_activity = 0
+        else:
+            last_activity = slave_time - t
+
+    # If this was over 10 minutes ago
+    if (slave_time - t) > 10*60 and (slave_time - t) > uptime:
+        log.warning("%s - shut down happened %ss ago, but we've been up for %ss - %s", name, slave_time-t, uptime, line.strip())
+        # If longer than 30 minutes, try rebooting
+        if (slave_time - t) > 30*60:
+            log.warning("%s - rebooting", name)
+            stdin, stdout, stderr = client.exec_command("sudo reboot")
+            stdin.close()
+
+    log.debug("%s - %s - %s", name, last_activity, line.strip())
+    return last_activity
+
+
+def get_tacfile(client):
+    stdin, stdout, stderr = client.exec_command("cat /builds/slave/buildbot.tac")
+    stdin.close()
+    data = stdout.read()
+    return data
+
+
+def get_buildbot_master(client):
+    tacfile = get_tacfile(client)
+    host = re.search("^buildmaster_host = '(.*?)'$", tacfile, re.M)
+    port = re.search("^port = (\d+)", tacfile, re.M)
+    assert host and port
+    host = host.group(1)
+    port = int(port.group(1))
+    return host, port
+
+
+def graceful_shutdown(name, ip, client):
+    # Find out which master we're attached to by looking at buildbot.tac
+    log.debug("%s - looking up which master we're attached to", name)
+    host, port = get_buildbot_master(client)
+    # http port is pb port -1000
+    port -= 1000
+
+    url = "http://{host}:{port}/buildslaves/{name}/shutdown".format(host=host, port=port, name=name)
+    log.debug("%s - POSTing to %s", name, url)
+    requests.post(url, allow_redirects=False)
+
+
+def aws_stop_idle(secrets, passwords, regions, dryrun=False):
+    if not regions:
+        # Look at all regions
+        log.debug("loading all regions")
+        regions = [r.name for r in boto.ec2.regions(**secrets)]
+
+    min_running_by_type = 0
+
+    for r in regions:
+        log.debug("looking at region %s", r)
+        conn = boto.ec2.connect_to_region(r, **secrets)
+
+        instances = get_buildbot_instances(conn)
+        instances_by_type = {}
+        for i in instances:
+            # TODO: Check if launch_time is too old, and terminate the instance
+            # if it is
+            # NB can't turn this on until aws_create_instance is working properly (with ssh keys)
+            instances_by_type.setdefault(i.tags['moz-type'], []).append(i)
+
+        # Make sure min_running_by_type are kept running
+        for t in instances_by_type:
+            to_remove = instances_by_type[t][:min_running_by_type]
+            for i in to_remove:
+                log.debug("%s - keep running (min %i instances of type %s)", i.tags['Name'], min_running_by_type, i.tags['moz-type'])
+                instances.remove(i)
+
+        for i in instances:
+            name = i.tags['Name']
+            # TODO: Check with slavealloc
+
+            ip = i.private_ip_address
+            ssh_client = get_ssh_client(name, ip, passwords)
+            if not ssh_client:
+                continue
+            last_activity = get_last_activity(name, ssh_client)
+            if last_activity == "stopped":
+                # TODO: could be that the machine is just starting up....
+                if not dryrun:
+                    log.info("%s - stopping instance (launched %s)", name, i.launch_time)
+                    i.stop()
+                else:
+                    log.info("%s - would have stopped", name)
+                continue
+
+            if last_activity == "booting":
+                # Wait harder
+                continue
+
+            log.debug("%s - last activity %is ago", name, last_activity)
+            # Determine if the machine is idle for more than 10 minutes
+            if last_activity > 300:
+                if not dryrun:
+                    # Hit graceful shutdown on the master
+                    log.info("%s - starting graceful shutdown", name)
+                    graceful_shutdown(name, ip, ssh_client)
+
+                    # Check if we've exited right away
+                    if get_last_activity(name, ssh_client) == "stopped":
+                        log.info("%s - stopping instance", name)
+                        i.stop()
+                else:
+                    log.info("%s - would have started graceful shutdown", name)
+            else:
+                log.debug("%s - not stopping", name)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.set_defaults(
+        regions=[],
+        secrets=None,
+        passwords=None,
+        loglevel=logging.INFO,
+        dryrun=False,
+    )
+
+    parser.add_option("-r", "--region", action="append", dest="regions")
+    parser.add_option("-k", "--secrets", dest="secrets")
+    parser.add_option("-s", "--key-name", dest="key_name")
+    parser.add_option("-v", "--verbose", action="store_const", dest="loglevel", const=logging.DEBUG)
+    parser.add_option("-p", "--paswords", dest="passwords")
+    parser.add_option("--dry-run", action="store_true", dest="dryrun")
+
+    options, args = parser.parse_args()
+
+    logging.basicConfig(level=options.loglevel, format="%(asctime)s - %(message)s")
+    logging.getLogger("boto").setLevel(logging.WARN)
+    logging.getLogger("paramiko").setLevel(logging.WARN)
+    logging.getLogger('requests').setLevel(logging.WARN)
+
+    if not options.secrets:
+        parser.error("secrets are required")
+
+    if not options.passwords:
+        parser.error("passwords are required")
+
+    secrets = json.load(open(options.secrets))
+    passwords = json.load(open(options.passwords))
+
+    aws_stop_idle(secrets, passwords, options.regions, dryrun=options.dryrun)
---
+++ aws/aws_watch_pending.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+"""
+Watches pending jobs and starts or creates EC2 instances if required
+"""
+import re, time
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+import boto.ec2
+import sqlalchemy as sa
+
+from aws_create_instance import make_instances
+
+import logging
+log = logging.getLogger()
+
+
+def find_pending(db):
+    engine = sa.create_engine(db)
+    result = engine.execute(sa.text("""
+        SELECT buildername, count(*) FROM
+               buildrequests WHERE
+               complete=0 AND
+               claimed_at=0 AND
+               submitted_at > :yesterday AND
+               submitted_at < :toonew
+
+               GROUP BY buildername"""),
+               yesterday=time.time()-86400,
+               toonew=time.time()-60,
+    )
+    retval = result.fetchall()
+    return retval
+
+
+def aws_resume_instances(instance_type, count, regions, secrets):
+    "resume up to `count` stopped instances of the given type in the given regions"
+    started = 0
+    for region in regions:
+        conn = boto.ec2.connect_to_region(region, **secrets)
+        reservations = conn.get_all_instances(filters={
+            'tag:moz-type': instance_type,
+            'instance-state-name': 'stopped',
+        })
+        stopped_instances = []
+        for r in reservations:
+            for i in r.instances:
+                if not i.tags.get('moz-type') == instance_type:
+                    log.debug("skipping %s; wrong type (%s)", i, i.tags.get('moz-type'))
+                    continue
+                if i.state != 'stopped':
+                    log.debug("skipping %s; wrong state (%s)", i, i.state)
+                    continue
+                stopped_instances.append(i)
+
+        # Sort by launch_time so we can start most recently stopped instances first
+        stopped_instances.sort(key=lambda i: i.launch_time)
+        stopped_instances.reverse()
+        for i in stopped_instances:
+            log.info("%s - starting instance", i.tags['Name'])
+            i.start()
+            started += 1
+
+            if started == count:
+                return started
+
+    return started
+
+
+def aws_create_instances(instance_type, count, regions, secrets, key_name, instance_data):
+    instance_config = json.load(open("configs/%s" % instance_type))
+    max_count = instance_config['max_instances']
+
+    # Count how many we have in all regions
+    num = 0
+    instances = []
+    names = []
+    for region in regions:
+        conn = boto.ec2.connect_to_region(region, **secrets)
+        reservations = conn.get_all_instances(filters={'tag:moz-type': instance_type})
+        for r in reservations:
+            for i in r.instances:
+                if i.tags.get('moz-type') == instance_type and i.state != "terminated":
+                    instances.append(i)
+                    names.append(i.tags['Name'])
+                    num += 1
+
+    num_to_create = min(max_count - num, count)
+    log.info("%s - we have %i instances across all regions; we will create %i more (max is %i)", instance_type, num, num_to_create, max_count)
+
+    i = 1
+    to_create = []
+    while len(to_create) < num_to_create:
+        # Figure out its names
+        name = instance_config['hostname'] % i
+        if name not in names and name not in to_create:
+            to_create.append(name)
+        i += 1
+
+    log.info("%s - creating %s", instance_type, to_create)
+
+    # TODO do multi-region
+    if to_create:
+        make_instances(to_create, instance_config[regions[0]], regions[0], secrets, key_name, instance_data, create_ami=False)
+
+    return len(to_create)
+
+
+def aws_watch_pending(db, regions, secrets, key_name, instance_data, builder_map, allow_create):
+    # First find pending jobs in the db
+    pending = find_pending(db)
+
+    # Mapping of instance types to # of instances we want to creates
+    to_create = {}
+    # Then match them to the builder_map
+    for pending_buildername, count in pending:
+        for buildername_exp, instance_type in builder_map.items():
+            if re.match(buildername_exp, pending_buildername):
+                log.debug("%s has %i pending jobs, checking instances of type %s", pending_buildername, count, instance_type)
+                to_create[instance_type] = to_create.get(instance_type, 0) + count
+
+                break
+        else:
+            log.debug("%s has %i pending jobs, but no instance types defined", pending_buildername, count)
+
+    for instance_type, count in to_create.items():
+        log.debug("Need %i %s", count, instance_type)
+
+        # Check for stopped instances in the given regions and start them if there are any
+        started = aws_resume_instances(instance_type, count, regions, secrets)
+        count -= started
+        log.info("%s - started %i instances; need %i", instance_type, started, count)
+
+        # Then create new instances (subject to max_instances)
+        if allow_create:
+            created = aws_create_instances(instance_type, count, regions, secrets, key_name, instance_data)
+            count -= created
+            log.info("%s - created %i instances; need %i", instance_type, created, count)
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.set_defaults(
+        regions=[],
+        secrets=None,
+        loglevel=logging.INFO,
+        key_name=None,
+        instance_data=None,
+        config=None,
+        allow_create=False,
+    )
+
+    parser.add_option("-r", "--region", action="append", dest="regions")
+    parser.add_option("-k", "--secrets", dest="secrets")
+    parser.add_option("-s", "--key-name", dest="key_name")
+    parser.add_option("-v", "--verbose", action="store_const", dest="loglevel", const=logging.DEBUG)
+    parser.add_option("-i", "--instance-data", dest="instance_data")
+    parser.add_option("-c", "--config", dest="config")
+    parser.add_option("--allow-create", dest="allow_create", action="store_true")
+
+    options, args = parser.parse_args()
+
+    logging.basicConfig(level=options.loglevel, format="%(asctime)s - %(message)s")
+    logging.getLogger("boto").setLevel(logging.INFO)
+
+    if not options.regions:
+        options.regions = ['us-west-1']
+
+    if not options.secrets:
+        parser.error("secrets are required")
+
+    if not options.instance_data:
+        parser.error("instance data is required")
+
+    if not options.config:
+        parser.error("you must specify a config file to use")
+
+    config = json.load(open(options.config))
+    secrets = json.load(open(options.secrets))
+    instance_data = json.load(open(options.instance_data))
+    aws_watch_pending(
+        config['db'], options.regions, secrets, options.key_name,
+        instance_data, config['buildermap'], options.allow_create,
+    )
