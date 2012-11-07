f5ae11b6e476bd5bf68e1d673139586077fbcf15
Rail Aliiev <rail@mozilla.com>
2012-11-07 17:23:07 -0500

    Use centos mirrors only

--- aws/aws_create_puppetmaster.py
+++ aws/aws_create_puppetmaster.py
@@ -149,7 +149,7 @@ configs = {
             "repo_snapshot_id": "snap-d31a30ff",  # This will be mounted at /data
         },
         "us-east-1": {
-            "ami": "ami-c4e366ad",  # Centos6
+            "ami": "ami-b6ed68df",  # Centos6
             "subnet_id": "subnet-33a98358",
             "security_group_ids": ["sg-b36a84dc"],
             "instance_type": "m1.medium",
