126c1911e4bba4172482df6b078531a16142949d
Rail Aliiev <rail@mozilla.com>
2013-05-09 07:54:00 -0400

    Update puppetmaster snapshots

--- aws/aws_create_puppetmaster.py
+++ aws/aws_create_puppetmaster.py
@@ -149,14 +149,14 @@ configs = {
             "subnet_id": ["subnet-33a98358", "subnet-35a9835e", " subnet-0aa98361"],
             "security_group_ids": ["sg-b36a84dc"],
             "instance_type": "m1.large",
-            "repo_snapshot_id": "snap-4177ed1a",  # This will be mounted at /data
+            "repo_snapshot_id": "snap-e9399cb3",  # This will be mounted at /data
         },
         "us-west-2": {
             "ami": "ami-16d15926",
             "subnet_id": ["subnet-b948dad0", "subnet-ba48dad3", "subnet-bf48dad6"],
             "security_group_ids": ["sg-4e2d3022"],
             "instance_type": "m1.large",
-            "repo_snapshot_id": "snap-4a547c72",  # This will be mounted at /data
+            "repo_snapshot_id": "snap-a92e2c91",  # This will be mounted at /data
         },
     },
 }
