ecefc4e7d76445b644514597f87921a1d2cb013b
Rail Aliiev <rail@mozilla.com>
2013-06-21 10:38:53 -0400

    Add free_ip.py utility

---
+++ aws/free_ips.py
@@ -0,0 +1,52 @@
+from boto.ec2 import connect_to_region
+from boto.vpc import VPCConnection
+from IPy import IP
+import random
+import argparse
+import json
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--config", required=True,
+                    type=argparse.FileType('r'),
+                    help="instance configuration to use")
+parser.add_argument("-r", "--region", help="region to use",
+                    default="us-east-1")
+parser.add_argument("-k", "--secrets", type=argparse.FileType('r'),
+                    help="optional file where secrets can be found")
+parser.add_argument("-n", "--number", type=int, required=True,
+                    help="How many IPs you need")
+args = parser.parse_args()
+
+try:
+    config = json.load(args.config)[args.region]
+except KeyError:
+    parser.error("unknown configuration")
+
+if args.secrets:
+    secrets = json.load(args.secrets)
+    conn = connect_to_region(
+        args.region,
+        aws_access_key_id=secrets['aws_access_key_id'],
+        aws_secret_access_key=secrets['aws_secret_access_key']
+    )
+else:
+    conn = connect_to_region(args.region)
+
+vpc = VPCConnection(region=conn.region)
+
+res = conn.get_all_instances()
+instances = reduce(lambda a, b: a + b, [r.instances for r in res])
+used_ips = [i.private_ip_address for i in instances]
+
+subnets = vpc.get_all_subnets(subnet_ids=config["subnet_ids"])
+blocks = [s.cidr_block for s in subnets]
+
+available_ips = []
+for b in blocks:
+    for ip in IP(b):
+        if str(ip) not in used_ips:
+            available_ips.append(ip)
+sample = random.sample(available_ips, args.number)
+for ip in sample:
+    print ip
