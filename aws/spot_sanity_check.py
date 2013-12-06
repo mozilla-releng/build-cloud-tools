c210616e6a946a144c162e2771eba7c375038ffc
Rail Aliiev <rail@mozilla.com>
2013-12-06 12:35:22 -0500

    Bug 935533 - Prepare infra to handle spot instances

--- aws/spot_sanity_check.py
+++ aws/spot_sanity_check.py
@@ -3,10 +3,145 @@
 import argparse
 import json
 import logging
+import datetime
+
 from boto.ec2 import connect_to_region
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime, Float, Integer, \
+    create_engine, ForeignKey
+from sqlalchemy.orm import validates, relationship, sessionmaker
 
 log = logging.getLogger(__name__)
 REGIONS = ['us-east-1', 'us-west-2']
+Base = declarative_base()
+
+
+def aws_time_to_gm(aws_time):
+    t = datetime.datetime.strptime(aws_time[:-1] + "UTC",
+                                   '%Y-%m-%dT%H:%M:%S.000%Z')
+    return t
+
+
+def stringify_dict(d):
+    return json.dumps(d)
+
+
+class SpotRequest(Base):
+    __tablename__ = 'spot_requests'
+    id = Column(String, primary_key=True)
+    create_time = Column(DateTime)
+    instance_id = Column(String)
+    launched_availability_zone = Column(String)
+    price = Column(Float)
+    product_description = Column(String)
+    region = Column(String)
+    tags = Column(String)
+    states = relationship("SpotState")
+    statuses = relationship("SpotStatus")
+
+    @validates('create_time')
+    def convert_datetime(self, key, t):
+        if isinstance(t, basestring):
+            return aws_time_to_gm(t)
+        else:
+            return t
+
+    @validates('tags')
+    def stringify_tags(self, key, t):
+        if isinstance(t, basestring):
+            return t
+        else:
+            return stringify_dict(t)
+
+
+class SpotState(Base):
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    state = Column(String)
+    seen = Column(DateTime)
+    request_id = Column(String, ForeignKey('spot_requests.id'))
+
+    def __init__(self, state, seen):
+        self.state = state
+        self.seen = seen
+
+    @validates('seen')
+    def convert_datetime(self, key, t):
+        if isinstance(t, basestring):
+            return aws_time_to_gm(t)
+        else:
+            return t
+
+
+class SpotStatus(Base):
+    __tablename__ = "statuses"
+    id = Column(Integer, primary_key=True)
+    code = Column(String)
+    message = Column(String)
+    update_time = Column(DateTime)
+    request_id = Column(String, ForeignKey('spot_requests.id'))
+
+    def __init__(self, code, message, update_time):
+        self.code = code
+        self.message = message
+        self.update_time = update_time
+
+    @validates('update_time')
+    def convert_datetime(self, key, t):
+        if isinstance(t, basestring):
+            return aws_time_to_gm(t)
+        else:
+            return t
+
+
+def cancel_low_price(conn):
+    spot_requests = conn.get_all_spot_instance_requests() or []
+    for req in spot_requests:
+        if req.state == "open" and req.status.code == "price-too-low":
+            log.warning("Cancelling price-too-low request %s", req)
+            req.cancel()
+
+
+def update_spot_stats(conn, session):
+    for req in conn.get_all_spot_instance_requests():
+        r = session.query(SpotRequest).filter(SpotRequest.id == req.id).first()
+        if r:
+            if r.instance_id != req.instance_id:
+                log.debug("Update instance id %s", req.instance_id)
+                r.instance_id = req.instance_id
+            if r.launched_availability_zone != req.launched_availability_zone:
+                r.launched_availability_zone = req.launched_availability_zone
+            if r.tags != stringify_dict(req.tags):
+                log.debug("Update tags: %s", req.tags)
+                r.tags = req.tags
+
+            if req.state not in [s.state for s in r.states]:
+                log.debug("New state: %s", req.state)
+                r.states.append(
+                    SpotState(req.state, datetime.datetime.utcnow()))
+            if req.status.code not in [s.code for s in r.statuses]:
+                log.debug("New status: %s <%s> on %s", req.status.code,
+                          req.status.message, req.status.update_time)
+                r.states.append(
+                    SpotStatus(req.status.code, req.status.message,
+                               aws_time_to_gm(req.status.update_time)))
+        else:
+            log.debug("New request: %s", req.id)
+            r = SpotRequest()
+            r.id = req.id
+            r.create_time = req.create_time
+            r.instance_id = req.instance_id
+            r.launched_availability_zone = req.launched_availability_zone
+            r.price = req.price
+            r.product_description = req.product_description
+            r.region = req.region.name
+            r.tags = req.tags
+            r.states = [SpotState(req.state, datetime.datetime.utcnow())]
+            r.statuses = [SpotStatus(req.status.code, req.status.message,
+                                     aws_time_to_gm(req.status.update_time))]
+            session.add(r)
+
+        session.commit()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -16,6 +151,7 @@ if __name__ == '__main__':
                         help="optional list of regions")
     parser.add_argument("-q", "--quiet", action="store_true",
                         help="Supress logging messages")
+    parser.add_argument("-d", "--db", default="spots.db")
 
     args = parser.parse_args()
     if args.secrets:
@@ -29,6 +165,11 @@ if __name__ == '__main__':
     else:
         log.setLevel(logging.ERROR)
 
+    engine = create_engine('sqlite:///%s' % args.db)
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     if not args.regions:
         args.regions = REGIONS
     for region in args.regions:
@@ -40,9 +181,5 @@ if __name__ == '__main__':
             )
         else:
             conn = connect_to_region(region)
-
-        spot_requests = conn.get_all_spot_instance_requests() or []
-        for req in spot_requests:
-            if req.state == "open" and req.status.code == "price-too-low":
-                log.warning("Cancelling price-too-low request %s", req)
-                req.cancel()
+        cancel_low_price(conn)
+        update_spot_stats(conn, session)
