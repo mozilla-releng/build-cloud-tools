46a41915432c12f3e3696a4b0cd96124acdb31ea
Chris AtLee <catlee@mozilla.com>
2014-02-20 09:10:14 -0500

    Tools to manage security groups and routing tables

---
+++ aws/aws_manage_routingtables.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+import socket
+
+import boto.vpc
+import yaml
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def get_connection(region, aws_access_key_id=None, aws_secret_access_key=None):
+    return boto.vpc.connect_to_region(
+        region,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+
+def load_config(filename):
+    return yaml.load(open(filename))
+
+
+_dns_cache = {}
+def resolve_host(hostname):
+    if hostname in _dns_cache:
+        return _dns_cache[hostname]
+    log.info("resolving host %s", hostname)
+    ip = socket.gethostbyname(hostname)
+    _dns_cache[hostname] = ip
+    return ip
+
+
+def sync_tables(conn, my_tables, remote_tables):
+    # Check that remote tables don't have overlapping names
+    seen_names = set()
+    for t in remote_tables[:]:
+        name = t.tags.get('Name')
+        if not name:
+            log.warn("table %s has no name", t.id)
+            remote_tables.remove(t)
+            continue
+        if name in seen_names:
+            log.warn("table %s has a duplicate name %s; skipping", t.id, name)
+            remote_tables.remove(t)
+        seen_names.add(name)
+
+    for name in my_tables:
+        if name not in seen_names:
+            # TODO: Allow multiple VPCs to exist per region
+            vpc_id = conn.get_all_vpcs()[0].id
+            log.info("remote table %s doesn't exist; creating in vpc %s", name, vpc_id)
+            t = conn.create_route_table(vpc_id)
+            t.add_tag('Name', name)
+            remote_tables.append(t)
+
+    # Sync remote tables
+    for t in remote_tables:
+        name = t.tags['Name']
+        if name not in my_tables:
+            if raw_input("table %s doesn't exist in local config; delete? (y/N)" % t.id) == 'y':
+                log.warn("DELETING %s", t.id)
+                # TODO
+            continue
+
+        my_t = my_tables[name]
+
+        # Now look at routes
+        remote_routes = set()
+        for r in t.routes:
+            remote_routes.add((r.destination_cidr_block, r.gateway_id, r.instance_id))
+
+        my_routes = set()
+        IGW = None
+        VGW = None
+        for cidr, dest in my_t['routes'].iteritems():
+            if "/" not in cidr:
+                cidr = resolve_host(cidr) + "/32"
+            instance_id = None
+            gateway_id = None
+            if dest == "IGW":
+                # Use our VPC's IGW
+                if IGW is None:
+                    IGW = conn.get_all_internet_gateways()[0]
+                gateway_id = IGW.id
+            elif dest == "VGW":
+                # Use our VPC's IGW
+                if VGW is None:
+                    VGW = conn.get_all_vpn_gateways()[0]
+                gateway_id = VGW.id
+            elif dest == 'local':
+                gateway_id = 'local'
+            elif dest and dest.startswith("i-"):
+                instance_id = dest
+            my_routes.add((cidr, gateway_id, instance_id))
+
+        # Delete extra routes first, in case we need to change the gateway of
+        # some route
+        extra_routes = remote_routes - my_routes
+        for cidr, gateway_id, instance_id in extra_routes:
+            log.info("%s - deleting route to %s via %s %s", t.id, cidr, gateway_id, instance_id)
+            if raw_input("delete? (y/N) ") == 'y':
+                conn.delete_route(t.id, cidr)
+
+        # Add missing routes
+        missing_routes = my_routes - remote_routes
+        for cidr, gateway_id, instance_id in missing_routes:
+            log.info("%s - adding route to %s via %s %s", t.id, cidr, gateway_id, instance_id)
+            conn.create_route(t.id, cidr, gateway_id=gateway_id, instance_id=instance_id)
+
+        # TODO: Set default, manage subnets
+
+
+def main():
+    import sys
+    log.debug("Parsing file")
+    rt_defs = load_config(sys.argv[1])
+
+    regions = set(rt_defs.keys())
+
+    log.info("Working in regions %s", regions)
+
+    for region in regions:
+        log.info("Working in %s", region)
+        conn = get_connection(region)
+        remote_tables = conn.get_all_route_tables()
+
+        # Compare vs. our configs
+        my_tables = rt_defs[region]
+
+        sync_tables(conn, my_tables, remote_tables)
+
+if __name__ == '__main__':
+    logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+    main()
---
+++ aws/aws_manage_securitygroups.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+import socket
+
+import boto.ec2
+import yaml
+
+import logging
+log = logging.getLogger(__name__)
+
+
+def get_connection(region, aws_access_key_id=None, aws_secret_access_key=None):
+    return boto.ec2.connect_to_region(
+        region,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
+    )
+
+
+def load_config(filename):
+    return yaml.load(open(filename))
+
+
+def get_remote_sg_by_name(groups, name):
+    log.info("Looking for sg %s", name)
+    for g in groups:
+        if g.name == name:
+            log.info("Found %s", g)
+            return g
+    log.info("Didn't find %s; returning None", name)
+
+
+def make_rules_for_def(rule):
+    """Returns a set of rules for a given config definition. A rule is a
+    (proto, from_port, to_port, hosts) tuple
+    """
+    retval = []
+    proto = str(rule['proto'])
+    if 'ports' in rule:
+        ports = [str(p) for p in rule['ports']]
+    else:
+        ports = [None]
+    hosts = rule['hosts']
+    # Resolve the hostnames
+    # TODO: find all IPs, not just the first one returned
+    log.debug("%s %s %s", proto, ports, hosts)
+    log.debug("Resolving hostnames")
+    for h in hosts[:]:
+        if '/' not in h:
+            ip = socket.gethostbyname(h)
+            hosts.remove(h)
+            hosts.append("%s/32" % ip)
+    log.debug("%s %s %s", proto, ports, hosts)
+
+    for port in ports:
+        retval.append((proto, port, port, set(hosts)))
+    return retval
+
+
+def make_rules(sg_config):
+    rules = {}
+    for rule_def in sg_config.get('inbound', []):
+        for proto, from_port, to_port, hosts in make_rules_for_def(rule_def):
+            rules.setdefault(('inbound', proto, from_port, to_port), set()).update(hosts)
+
+    for rule_def in sg_config.get('outbound', []):
+        for proto, from_port, to_port, hosts in make_rules_for_def(rule_def):
+            rules.setdefault(('outbound', proto, from_port, to_port), set()).update(hosts)
+
+    return rules
+
+
+def rules_from_sg(sg):
+    rules = {}
+    for rule in sg.rules:
+        rules.setdefault(('inbound', rule.ip_protocol, rule.from_port, rule.to_port), set()).update(set(g.cidr_ip for g in rule.grants))
+    for rule in sg.rules_egress:
+        rules.setdefault(('outbound', rule.ip_protocol, rule.from_port, rule.to_port), set()).update(set(g.cidr_ip for g in rule.grants))
+
+    return rules
+
+
+def add_hosts(sg, rule_key, hosts):
+    if rule_key[0] == 'inbound':
+        auth_func = sg.connection.authorize_security_group
+    else:
+        auth_func = sg.connection.authorize_security_group_egress
+
+    for h in hosts:
+        auth_func(
+            group_id=sg.id,
+            ip_protocol=rule_key[1],
+            from_port=rule_key[2],
+            to_port=rule_key[3],
+            cidr_ip=h,
+        )
+
+
+def remove_hosts(sg, rule_key, hosts):
+    if rule_key[0] == 'inbound':
+        auth_func = sg.connection.revoke_security_group
+    else:
+        auth_func = sg.connection.revoke_security_group_egress
+
+    for h in hosts:
+        auth_func(
+            group_id=sg.id,
+            ip_protocol=rule_key[1],
+            from_port=rule_key[2],
+            to_port=rule_key[3],
+            cidr_ip=h,
+        )
+
+
+def sync_security_group(remote_sg, sg_config, prompt):
+    rules = make_rules(sg_config)
+    remote_rules = rules_from_sg(remote_sg)
+
+    # Check if we need to add any rules
+    for rule_key, hosts in rules.items():
+        new_hosts = hosts - remote_rules.get(rule_key, set())
+        if new_hosts:
+            if prompt and raw_input("%s - Add rule for %s to %s? (y/N) " % (remote_sg.name, rule_key, new_hosts)) != 'y':
+                continue
+            log.info("%s - adding rule for %s to %s", remote_sg.name, rule_key, new_hosts)
+            add_hosts(remote_sg, rule_key, new_hosts)
+
+    # Now check if we should delete any rules
+    for rule_key, hosts in remote_rules.items():
+        old_hosts = hosts - rules.get(rule_key, set())
+        if old_hosts:
+            if prompt and raw_input("%s - Delete rule %s to %s (y/N) " % (remote_sg.name, rule_key, old_hosts)) != 'y':
+                continue
+            log.info("%s - removing rule for %s to %s", remote_sg.name, rule_key, new_hosts)
+            remove_hosts(remote_sg, rule_key, old_hosts)
+
+
+def main():
+    import sys
+    log.debug("Parsing file")
+    sg_defs = load_config(sys.argv[1])
+
+    # Get the security groups for all affected regions
+    regions = set()
+    for sg_name, sg_config in sg_defs.items():
+        regions.update(sg_config['regions'])
+
+    log.info("Working in regions %s", regions)
+
+    security_groups_by_region = {}
+    connections_by_region = {}
+    for region in regions:
+        log.info("Loading groups for %s", region)
+        conn = get_connection(region)
+        all_groups = conn.get_all_security_groups()
+        connections_by_region[region] = conn
+        security_groups_by_region[region] = all_groups
+
+    prompt = True
+
+    # Now compare vs. our configs
+    for sg_name, sg_config in sg_defs.items():
+        for region in sg_config['regions']:
+            log.info("Working in %s", region)
+            remote_sg = get_remote_sg_by_name(security_groups_by_region[region], sg_name)
+            if not remote_sg:
+                if prompt:
+                    if raw_input('Create security group %s in %s? (y/N) ' % (sg_name, region)) != 'y':
+                        log.info("Exiting")
+                        exit(0)
+                log.info("Creating group %s", sg_name)
+                remote_sg = connections_by_region[region].create_security_group(
+                    sg_name,
+                    vpc_id=sg_config['regions'][region],
+                    description=sg_config['description'],
+                )
+
+            sync_security_group(remote_sg, sg_config, prompt=prompt)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.INFO)
+    main()
