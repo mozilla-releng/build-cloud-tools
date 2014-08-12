ef2167cfb38d907f63a475137eb9309424af273d
Rail Aliiev <rail@mozilla.com>
2014-08-12 10:20:32 -0400

    Bug 1050281 - Clean up / handle case where spot request is active but instance has gone away

--- scripts/spot_sanity_check.py
+++ scripts/spot_sanity_check.py
@@ -6,6 +6,7 @@ import logging
 import datetime
 import site
 import os
+import time
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, DateTime, Float, Integer, \
@@ -13,7 +14,8 @@ from sqlalchemy import Column, String, DateTime, Float, Integer, \
 from sqlalchemy.orm import validates, relationship, sessionmaker
 
 site.addsitedir(os.path.join(os.path.dirname(__file__), ".."))
-from cloudtools.aws import get_aws_connection, DEFAULT_REGIONS
+from cloudtools.aws import get_aws_connection, DEFAULT_REGIONS, \
+    parse_aws_time, aws_get_all_instances
 from cloudtools.aws.spot import CANCEL_STATUS_CODES, IGNORABLE_STATUS_CODES
 
 log = logging.getLogger(__name__)
@@ -98,8 +100,16 @@ class SpotStatus(Base):
             return t
 
 
-def cancel_low_price(conn):
-    spot_requests = conn.get_all_spot_instance_requests() or []
+def sanity_check(regions):
+    spot_requests = []
+    for r in regions:
+        conn = get_aws_connection(r)
+        region_spot_requests = conn.get_all_spot_instance_requests()
+        if region_spot_requests:
+            spot_requests.extend(region_spot_requests)
+    all_spot_instances = aws_get_all_instances(regions)
+    instance_ids = [i.id for i in all_spot_instances]
+
     for req in spot_requests:
         if req.state in ["open", "failed"]:
             if req.status.code in CANCEL_STATUS_CODES:
@@ -109,6 +119,14 @@ def cancel_low_price(conn):
             elif req.status.code not in IGNORABLE_STATUS_CODES:
                 log.error("Uknown status for request %s: %s", req,
                           req.status.code)
+        # Cancel all active request older than 30 mins without runing instances
+        elif req.state == "active" and \
+                parse_aws_time(req.create_time) + 30 * 60 < time.time() and \
+                req.instance_id not in instance_ids:
+            log.info("Cancelling request %s: %s is not running", req,
+                     req.instance_id)
+            req.add_tag("moz-cancel-reason", "no-running-instances")
+            req.cancel()
 
 
 def update_spot_stats(conn, session):
@@ -179,4 +197,4 @@ if __name__ == '__main__':
     for region in args.regions:
         conn = get_aws_connection(region)
         update_spot_stats(conn, session)
-        cancel_low_price(conn)
+    sanity_check(args.regions)
