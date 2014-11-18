b0cbc401a7ee3ca5b7daea903f3177af5a4dd263
Rail Aliiev <rail@mozilla.com>
2014-11-18 13:21:55 -0500

    Bug 1092872 - Reduce boto spamminess

--- scripts/tag_spot_instances.py
+++ scripts/tag_spot_instances.py
@@ -28,6 +28,7 @@ if __name__ == '__main__':
         log.setLevel(logging.DEBUG)
     else:
         log.setLevel(logging.ERROR)
+        logging.getLogger("boto").setLevel(logging.CRITICAL)
 
     if not args.regions:
         args.regions = DEFAULT_REGIONS
