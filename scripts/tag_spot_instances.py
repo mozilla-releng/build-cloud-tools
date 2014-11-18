79425eab2bfc3b52f236d9e35dd5956f789e23f3
Rail Aliiev <rail@mozilla.com>
2014-11-18 10:11:22 -0500

    iBug 1092872 - Lots of RequestResourceCountExceeded errors from AWS in cron mail for tag_spot_instances.py

--- scripts/tag_spot_instances.py
+++ scripts/tag_spot_instances.py
@@ -4,58 +4,13 @@ import argparse
 import logging
 import site
 import os
-from threading import Thread
-from Queue import Queue, Empty
 
 site.addsitedir(os.path.join(os.path.dirname(__file__), ".."))
 from cloudtools.aws import DEFAULT_REGIONS
-from cloudtools.aws.spot import get_spot_instances, get_spot_request
+from cloudtools.aws.spot import get_instances_to_tag, \
+    populate_spot_requests_cache, copy_spot_request_tags
 
 log = logging.getLogger(__name__)
-
-
-def tag_it(i):
-    log.debug("Tagging %s", i)
-    req = get_spot_request(i.region.name, i.spot_instance_request_id)
-    if not req:
-        log.error("Cannot find spot request for %s", i)
-        return
-    tags = {}
-    for tag_name, tag_value in req.tags.iteritems():
-        if not tag_name in i.tags:
-            log.info("Adding '%s' tag with '%s' value to %s", tag_name,
-                     tag_value, i)
-            tags[tag_name] = tag_value
-    tags["moz-state"] = "ready"
-    i.connection.create_tags([i.id], tags)
-
-
-def tagging_worker(q):
-    while True:
-        try:
-            i = q.get(timeout=0.1)
-        except Empty:
-            log.debug("Exiting worker...")
-            return
-        try:
-            tag_it(i)
-        except:
-            log.debug("Failed to tag %s", i, exc_info=True)
-
-
-def populate_queue(region, q):
-    log.debug("Getting all spot instances in %s...", region)
-    all_spot_instances = get_spot_instances(region)
-    for i in all_spot_instances:
-        name = i.tags.get('Name')
-        fqdn = i.tags.get('FQDN')
-        moz_type = i.tags.get('moz-type')
-        moz_state = i.tags.get('moz-state')
-        # If one of the tags is unset/empty
-        if not all([name, fqdn, moz_type, moz_state]):
-            log.debug("Adding %s in %s to queue", i, region)
-            q.put(i)
-    log.debug("Done with %s", region)
 
 
 if __name__ == '__main__':
@@ -77,22 +32,12 @@ if __name__ == '__main__':
     if not args.regions:
         args.regions = DEFAULT_REGIONS
 
-    q = Queue()
-
-    threads = [Thread(target=populate_queue, args=(r, q)) for r in
-               args.regions]
-    log.debug("Waiting for regions...")
-    for t in threads:
-        t.start()
-    for t in threads:
-        t.join(timeout=60)
-
-    if not q.empty():
-        num_threads = min(args.concurrency, q.qsize())
-        threads = [Thread(target=tagging_worker, args=(q,)) for _ in
-                   range(num_threads)]
-        log.debug("Waiting for %s workers...", num_threads)
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join(timeout=240)
+    instances_to_tag = []
+    for r in args.regions:
+        instances_to_tag.extend(get_instances_to_tag(r))
+        if instances_to_tag:
+            populate_spot_requests_cache(
+                r, [i.spot_instance_request_id for i in instances_to_tag])
+            for i in instances_to_tag:
+                log.debug("tagging %s", i)
+                copy_spot_request_tags(i)
