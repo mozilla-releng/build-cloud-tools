34f7f5d9f3b122eb2cda96bbf6754af25ed63bec
Dustin J. Mitchell <dustin@mozilla.com>
2015-01-13 09:59:43 -0500

    refactor to organize stacks with a stacks.yml

--- scripts/aws_deploy_stack.py
+++ scripts/aws_deploy_stack.py
@@ -5,58 +5,73 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import argparse
-import datetime
+import boto.cloudformation
+import boto.s3
 import sys
 import time
 import os
 import logging
-import boto.cloudformation
-import boto.s3
+import yaml
 
 from cfn_pyplates.cli import _find_cloudformationtemplate
 from cfn_pyplates.cli import _load_pyplate
 from cfn_pyplates.options import OptionsMapping
 
-import site
-site.addsitedir(os.path.join(os.path.dirname(__file__), ".."))
-
 log = logging.getLogger(__name__)
-BUCKET_NAME = 'mozilla-releng-cloudformation-templates-{}'
+
 
 def main():
-    parser = argparse.ArgumentParser(description='deploy a cloudformation template')
-    parser.add_argument('template',
-            help='cft_pyplates template to deploy')
-    parser.add_argument('--region', type=str,
-            help='AWS region to deploy to',
-            required=True)
-    parser.add_argument('--stack', type=str,
-            help='CloudFormation stack to create or update',
-            required=True)
+    stacks_yml = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "../configs/cloudformation/stacks.yml"))
+    parser = argparse.ArgumentParser(description='deploy a cloudformation stack')
+    parser.add_argument('stack', type=str,
+                        help='CloudFormation stack to create or update')
+    parser.add_argument('--config', type=str,
+                        help='Path to stacks.yml (default is relative to this script)',
+                        default=stacks_yml)
     parser.add_argument('--create', action='store_true',
-            help='Create a new stack, instead of updating')
+                        help='Create a new stack, instead of updating')
     parser.add_argument('--noop', action='store_true',
-            help='Just parse and output the template, without updating')
+                        help='Just parse and output the template, without updating')
     parser.add_argument('--wait', action='store_true',
-            help='Wait for the create or update operation to complete')
+                        help='Wait for the create or update operation to complete')
 
     args = parser.parse_args()
+    if not args.stack:
+        parser.error("No stack specified")
 
-    template = args.template
-    log.debug("converting template %r to JSON", template)
-    
-    # see https://github.com/seandst/cfn-pyplates/issues/27 for the
-    # solution to using these private functions
-    sys.path.insert(0, os.path.dirname(template))
-    options_mapping = OptionsMapping({})
-    pyplate = _load_pyplate(open(template), options_mapping)
-    cft = _find_cloudformationtemplate(pyplate)
+    # load the config file
+    config = yaml.load(open(args.config))
+    if 'stacks' not in config or args.stack not in config['stacks']:
+        parser.error("Stack %r not found in %s" % (args.stack, args.config))
+    if not deploy_stack(args, config, args.stack):
+        sys.exit(1)
+
+
+def deploy_stack(args, config, stack_name):
+    stack_config = config['stacks'][stack_name]
+    template_path = os.path.join(os.path.dirname(args.config),
+                                 stack_config['template'])
+    template_body = load_template(args, stack_config, template_path)
     if args.noop:
-        print unicode(cft)
+        print template_body
         return
 
-    if not deploy_template(args, unicode(cft)):
-        sys.exit(1)
+    return deploy_template(args, stack_config, template_body)
+
+
+def load_template(args, stack_config, template_path):
+    log.debug("converting template %r to JSON", template_path)
+    # see https://github.com/seandst/cfn-pyplates/issues/27 for the
+    # solution to using these private functions
+    sys.path.insert(0, os.path.dirname(template_path))
+    options_mapping = OptionsMapping({})
+    pyplate = _load_pyplate(open(template_path), options_mapping)
+    cft = _find_cloudformationtemplate(pyplate)
+    return unicode(cft)
+
 
 class EventLoop(object):
 
@@ -68,7 +83,7 @@ class EventLoop(object):
 
     def iterate(self, log_events=True):
         evts = self.conn.describe_stack_events(
-                self.stackid, next_token=self.next_token)
+            self.stackid, next_token=self.next_token)
         self.next_token = evts.next_token
         for evt in evts:
             if evt.event_id in self.seen:
@@ -83,8 +98,9 @@ class EventLoop(object):
                 log.info(msg)
 
 
-def deploy_template(args, template_body):
-    cf = boto.cloudformation.connect_to_region(args.region)
+def deploy_template(args, stack_config, template_body):
+    region = stack_config['region']
+    cf = boto.cloudformation.connect_to_region(region)
     if args.create:
         stackid = cf.create_stack(stack_name=args.stack,
                                   template_body=template_body)
@@ -107,26 +123,6 @@ def deploy_template(args, template_body):
                 return False
             return True
         time.sleep(2)
-
-def upload_template(args, tpl_json):
-    s3 = boto.s3.connect_to_region(args.region)
-
-    bucket_name = BUCKET_NAME.format(args.region)
-    location = '' if args.region == 'us-east-1' else args.region
-    bucket = s3.lookup(bucket_name)
-    if not bucket:
-        bucket = s3.create_bucket(bucket_name, location=location)
-        bucket.set_acl('private')
-
-    now = datetime.datetime.now().isoformat()
-    tpl_base = os.path.basename(args.template)[:-3]
-    key_name = '{}/{}.json'.format(tpl_base, now)
-    key = bucket.new_key(key_name)
-
-    key.set_contents_from_string(tpl_json)
-
-    region_host = s3.host
-    return 'https://{}/{}/{}'.format(region_host, bucket_name, key_name)
 
 if __name__ == '__main__':
     logging.basicConfig(format="%(asctime)s - %(message)s", level=logging.DEBUG)
