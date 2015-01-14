fb7061999efa26b82149c52644cd8ddf333e6ad4
Dustin J. Mitchell <dustin@mozilla.com>
2015-01-14 09:15:12 -0500

    fix pep8

--- configs/cloudformation/network.py
+++ configs/cloudformation/network.py
@@ -3,12 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 from cfn_pyplates.core import CloudFormationTemplate, Resource, Properties
-from cfn_pyplates.functions import ref
 
 cft = CloudFormationTemplate(description="Release Engineering network configuration")
 
-cft.resources.vpc = Resource('RelengVPC', 'AWS::EC2::VPC',
-        Properties({
-            'CidrBlock': '192.168.1.0/24',
-        })
-    )
+cft.resources.vpc = Resource(
+    'RelengVPC', 'AWS::EC2::VPC',
+    Properties({
+        'CidrBlock': '192.168.1.0/24',
+    })
+)
