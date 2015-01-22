fa230761fb2252fbcd41fc4c9cd711c4c72792a3
Dustin J. Mitchell <dustin@mozilla.com>
2015-01-22 10:27:01 -0500

    refactor aws_deploy_stack into a class for more state-sharing

--- cloudtools/tests/test_scripts_aws_deploy_stack.py
+++ cloudtools/tests/test_scripts_aws_deploy_stack.py
@@ -21,27 +21,23 @@ def test_deploy_not_found(tmpdir):
             raise RuntimeError
         error.side_effect = side_effect
         assert_raises(RuntimeError, lambda:
-                      aws_deploy_stack.deploy(['--config', str(config), 'NoSuchStack']))
+                      aws_deploy_stack.Deployer(['--config', str(config), 'NoSuchStack']))
         error.assert_called_with("Stack 'NoSuchStack' not found in " + str(config))
 
 
 def test_deploy_deploy(tmpdir):
     config = tmpdir.join('stacks.yml')
     config.write(STACKS_YML)
-    with mock.patch('cloudtools.scripts.aws_deploy_stack.deploy_stack') as deploy_stack:
-        assert aws_deploy_stack.deploy(['--config', str(config), 'MyStack'])
-        deploy_stack.assert_called_with(
-            mock.ANY,
-            {'region': 'us-west-1', 'template': 'my.py'},
-            'MyStack')
+    with mock.patch('cloudtools.scripts.aws_deploy_stack.Deployer.deploy_stack') as deploy_stack:
+        d = aws_deploy_stack.Deployer(['--config', str(config), 'MyStack'])
+        d.run()
+        deploy_stack.assert_called_with('MyStack')
 
 
 def test_deploy_delete(tmpdir):
     config = tmpdir.join('stacks.yml')
     config.write(STACKS_YML)
-    with mock.patch('cloudtools.scripts.aws_deploy_stack.delete_stack') as delete_stack:
-        assert aws_deploy_stack.deploy(['--config', str(config), '--delete', 'MyStack'])
-        delete_stack.assert_called_with(
-            mock.ANY,
-            {'region': 'us-west-1', 'template': 'my.py'},
-            'MyStack')
+    with mock.patch('cloudtools.scripts.aws_deploy_stack.Deployer.delete_stack') as delete_stack:
+        d = aws_deploy_stack.Deployer(['--config', str(config), '--delete', 'MyStack'])
+        d.run()
+        delete_stack.assert_called_with('MyStack')
