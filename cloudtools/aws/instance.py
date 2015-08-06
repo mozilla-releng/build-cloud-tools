4968d00da2efc739b34fda40b604cdc5f1096300
Rob Thijssen <rthijssen@gmail.com>
2015-08-06 13:06:57 +0100

    Bug 1191717: userdata token replacement refactored to use a shared mechanism across platforms; r=rail

--- cloudtools/aws/instance.py
+++ cloudtools/aws/instance.py
@@ -13,7 +13,7 @@ from fabric.context_managers import cd
 from ..fabric import setup_fabric_env
 from ..dns import get_ip
 from . import wait_for_status, AMI_CONFIGS_DIR, get_aws_connection, \
-    get_user_data_tmpl, get_region_dns_atom
+    get_user_data_tmpl
 from .vpc import get_subnet_id, ip_available, get_vpc
 from boto.exception import BotoServerError, EC2ResponseError
 
@@ -309,14 +309,10 @@ def create_block_device_mapping(ami, device_map):
     return bdm
 
 
-def user_data_from_template(moz_instance_type, fqdn, region):
+def user_data_from_template(moz_instance_type, tokens):
     user_data = get_user_data_tmpl(moz_instance_type)
     if user_data:
-        user_data = user_data.format(
-            fqdn=fqdn,
-            region_dns_atom=get_region_dns_atom(region),
-            moz_instance_type=moz_instance_type)
-
+        user_data = user_data.format(**tokens)
     return user_data
 
 
