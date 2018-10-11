b82a141ed1f746fbdf5d82b710755085175fc441
Jake Watkins <jwatkins@mozilla.com>
2018-10-11 09:58:20 -0700

    Add cname for coalesce stagin app

--- terraform/base/route53.tf
+++ terraform/base/route53.tf
@@ -92,6 +92,14 @@ resource "aws_route53_record" "heroku-treestatus-cname-prod" {
 ## Heroku releng staging app cnames ##
 ######################################
 
+resource "aws_route53_record" "heroku-coalease-cname-stage" {
+    zone_id = "${aws_route53_zone.mozilla-releng.zone_id}"
+    name = "coalesce.staging.mozilla-releng.net"
+    type = "CNAME"
+    ttl = "180"
+    records = ["coalesce.staging.mozilla-releng.net.herokudns.com"]
+}
+
 resource "aws_route53_record" "heroku-mapper-cname-stage" {
     zone_id = "${aws_route53_zone.mozilla-releng.zone_id}"
     name = "mapper.staging.mozilla-releng.net"
